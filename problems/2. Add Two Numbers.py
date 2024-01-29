5990da0f02e1d20f7249993b8fe6bc981a2f1b11
taeyoung-lee-tpv-clt <58061933+taeyoung-lee-tpv-clt@users.noreply.github.com>
2024-01-29 23:43:56 +0900

    Create 2. Add Two Numbers.py

---
+++ problems/2. Add Two Numbers.py
@@ -0,0 +1,21 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class SolutQion:
+    def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+        d = n =ListNode(0)
+        num1 = num2 = ""
+        while l1:
+            num1 += str(l1.val)
+            l1 = l1.next
+        while l2:
+            num2 += str(l2.val)
+            l2 = l2.next
+        rst = str(int(num1[::-1]) + int(num2[::-1]))[::-1]
+        
+        for i in rst:
+            d.next = ListNode(i)
+            d = d.next
+        return n.next
