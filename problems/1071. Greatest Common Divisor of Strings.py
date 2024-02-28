b437b65faebd94a59d27da618f2793dac4266ecf
taeyoung-lee-tpv-clt <58061933+taeyoung-lee-tpv-clt@users.noreply.github.com>
2024-02-28 16:12:51 +0900

    Update 1071. Greatest Common Divisor of Strings.py

--- problems/1071. Greatest Common Divisor of Strings.py
+++ problems/1071. Greatest Common Divisor of Strings.py
@@ -4,3 +4,30 @@ class Solution:
             return ''
         from math import gcd
         return str1[:gcd(len(str1), len(str2))]
+
+class Solution:
+    def gcdOfStrings(self, str1: str, str2: str) -> str:
+
+        s1 = max(str1, str2, key = lambda x: len(x))
+        if s1 == str1:
+            s2 = str2
+        else:
+            s2 = str1
+        a1, a2 = len(s1), len(s2)
+
+        if s2 * (a1//a2) == s1:
+            return s2
+        
+        for i in range(2, a2//2 + 1):
+            if (a2 / i) % 1 == 0:
+                if s2[:a2//i]*i == s2:
+                    if s1[:a2//i]*int(i*a1/a2) == s1:
+                        if s2[:a2//i] == s1[:a2//i]:
+                            return s2[:a2//i] 
+
+        if s1.count(s1[0]) == a1:
+            if s2.count(s2[0]) == a2:
+                if s1[0] == s2[0]:
+                    return s1[0]
+
+        return ''
