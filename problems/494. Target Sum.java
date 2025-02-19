3122c92e35fd85a337e0e6c61dd7e34983afcdfe
taeyoung <58061933+taeyoung-lee-tpv-clt@users.noreply.github.com>
2025-02-19 16:06:18 +0900

    Update 494. Target Sum.java

--- problems/494. Target Sum.java
+++ problems/494. Target Sum.java
@@ -1,3 +1,24 @@
+// https://leetcode.com/problems/target-sum/description/
+// 제일 빨라 
+class Solution{
+  public int findTargetSumWays(int[] nums, int target){
+    int sum = 0;
+    for ( int i : nums) { sum += nums;}
+    if ( sum < target || (sum + target) % 2 != 0) { return 0 }
+    int targetSum = (sum + target) / 2
+    int[] dp = new int[targetSum + 1];
+    dp[0] = 1;
+
+    for(int i : nums){
+      for(int j = targetSum ; j >= i ; --j){
+        dp[j] += dp[j - num];
+      }
+    }
+    return dp[targetSum];
+  
+    }
+  }
+///////////////////
 class Solution {
     public int findTargetSumWays(int[] nums, int target) {
         return dfs(nums, target, 0, 0, 0);
